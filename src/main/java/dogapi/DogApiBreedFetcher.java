4631a02259180a2f9fda5cfd8f7c5e8761d43b77
Simone <Simone2005666@gmail.com>
2025-10-18 11:08:12 -0400

    finish task#1

--- src/main/java/dogapi/DogApiBreedFetcher.java
+++ src/main/java/dogapi/DogApiBreedFetcher.java
@@ -19,17 +19,37 @@ public class DogApiBreedFetcher implements BreedFetcher {
 
     /**
      * Fetch the list of sub breeds for the given breed from the dog.ceo API.
+     *
      * @param breed the breed to fetch sub breeds for
      * @return list of sub breeds for the given breed
      * @throws BreedNotFoundException if the breed does not exist (or if the API call fails for any reason)
      */
     @Override
     public List<String> getSubBreeds(String breed) {
+        String url = "https://dog.ceo/api/breed/" + breed + "/list";
+        Request request = new Request.Builder()
+                .url(url)
+                .build();
+        try (Response response = client.newCall(request).execute()) {
+            String body = response.body().string();
+            JSONObject json = new JSONObject(body);
+            String status = json.getString("status");
+            if (status.equals("error")) {
+                throw new BreedNotFoundException(json.getString("message"));
+            }
+            JSONArray message = json.getJSONArray("message");
+            List<String> subBreeds = new ArrayList<>();
+            for (int i = 0; i < message.length(); i++) {
+                subBreeds.add(message.getString(i));
+            }
+            return subBreeds;
+        } catch (IOException e) {
+            throw new BreedNotFoundException("Failed to fetch breed info: " + e.getMessage());
+        }
+    }
+}
         // TODO Task 1: Complete this method based on its provided documentation
         //      and the documentation for the dog.ceo API. You may find it helpful
         //      to refer to the examples of using OkHttpClient from the last lab,
         //      as well as the code for parsing JSON responses.
         // return statement included so that the starter code can compile and run.
-        return new ArrayList<>();
-    }
-}
